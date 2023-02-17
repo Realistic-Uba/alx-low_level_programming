929b28cc67a4a0b04044fa85bad01f403951da1e
Ubaezuonu Chinedu Emmanuel <ubaezuonue@gmail.com>
2023-02-16 16:23:45 -0800

    Task 8 fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <unistd.h>
 
 /**
-* main - Entry point
+* main - Entry point of the program
 *
-* Return: Always 0 (Success)
+* Return: Always 1 (Error)
 */
 int main(void)
 {
-	const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, msg, 59);
+	const char *quote = "and that piece of art is useful\" - ";
+		"Dora Korpar, 2015-10-19\n";
+
+	write(2, quote, 59);
 	return (1);
 }
