a8ca2d6f7a0e60a9c6cc2040d28b2d568319c1cc
Ubaezuonu Chinedu Emmanuel <ubaezuonue@gmail.com>
2023-02-16 16:42:52 -0800

    Fixed task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,6 @@
 */
 int main(void)
 {
-	const char *quote = "and that piece of art is useful\" - ";
-		"Dora Korpar, 2015-10-19\n";
-
-	write(2, quote, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
