bc59265a331511ec64672411c7e36f3803162ddf
Ubaezuonu Chinedu Emmanuel <ubaezuonue@gmail.com>
2023-02-17 17:09:24 -0800

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints all possible different combinations of three digits,
+ * separated by ", " and printed in ascending order, with three digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 8; i++)
+	{
+	for (j = i + 1; j < 9; j++)
+	{
+	for (k = j + 1; k <= 9; k++)
+	{
+		putchar(i + '0');
+		putchar(j + '0');
+		putchar(k + '0');
+	if (i != 7 || j != 8 || k != 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
