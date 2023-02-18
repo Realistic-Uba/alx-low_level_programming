bb1b77e47d5749286b18905da0af54574c39e77a
Ubaezuonu Chinedu Emmanuel <ubaezuonue@gmail.com>
2023-02-17 17:23:18 -0800

    Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints all possible combinations of two two-digit numbers,
+ * separated by a space and comma, and printed in ascending order
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+	for (j = i; j <= 99; j++)
+	{
+	if (i != j)
+	{
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+		putchar(' ');
+		putchar(' ');
+		putchar((j / 10) + '0');
+		putchar((j % 10) + '0');
+
+	if (i != 98 || j != 99)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
