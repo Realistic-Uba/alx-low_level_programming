573ad41bbf9015b1c7ae2651d09a7755b6199dfa
Ubaezuonu Chinedu Emmanuel <ubaezuonue@gmail.com>
2023-02-21 06:01:50 -0800

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -14,8 +14,8 @@ int main(void)
 
 	for (i = 0; str[i] != '\0; i++)
 	{
-		putchar(str[i]);
+		_putchar(str[i]);
 	}
-	putchar('\n');
+	_putchar('\n');
 	return 0;
 }
