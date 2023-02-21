02acd37e2347c7684394adaacb2a46e7b5d28fa8
Ubaezuonu Chinedu Emmanuel <ubaezuonue@gmail.com>
2023-02-21 14:35:53 -0800

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * @c: is a letter,lower or uppercase
+ *
+ * Return: prints 1 if uppercase or 0 if lower
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
