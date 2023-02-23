6a871b22d478e65460211abb724fa396540a6615
Ubaezuonu Chinedu Emmanuel <ubaezuonue@gmail.com>
2023-02-23 14:10:05 -0800

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ * description: A function that prints the numbers, from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
