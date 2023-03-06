bffaeb81b5ddcae908329148c07e49aa9f02653d
Ubaezuonu Chinedu Emmanuel <ubaezuonue@gmail.com>
2023-03-06 13:30:14 -0800

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr -  locates a character in a string.
+ * @s: A pointer to the string to be searched
+ * @c: The character to search for
+ *
+ * Return: s
+ */
+char *_strchr(char *s, char c)
+{
+
+	for (; *s != '\0';)
+	{
+	if (*s == c)
+		return (s);
+	s++;
+	}
+	return (NULL);
+}
