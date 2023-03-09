72b3259bbdb88996c91c1a0e2ce1eaeb0a7499b1
Ubaezuonu Chinedu Emmanuel <ubaezuonue@gmail.com>
2023-03-08 18:16:55 -0800

    5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,51 +1,37 @@
+#include <stdio.h>
 #include "main.h"
 
-/**
- * sqrt_helper - Helper function for _sqrt_recursion
- * @n: Input number
- * @low: Lowest possible value of the natural square root
- * @high: Highest possible value of the natural square root
- *
- * Return: Natural square root of n if found, -1 otherwise
- */
-int sqrt_helper(int n, int low, int high);
+int _sqrt(int n, int i);
 
 /**
- * _sqrt_recursion - Returns the natural square root of a number
- * @n: Input number
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to compute the square root of.
  *
- * Return: Natural square root of n if found, -1 otherwise
+ * Return: If n has a natural square root, return the square root.
+ *         If n does not have a natural square root, return -1.
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-	return (-1); /* Negative numbers don't have a natural square root */
-	return (sqrt_helper(n, 0, n));
-		/* Call helper function with initial low and high values */
+	return (_sqrt(n, 1));
 }
-
 /**
- * sqrt_helper - Helper function for _sqrt_recursion
- * @n: Input number
- * @low: Lowest possible value of the natural square root
- * @high: Highest possible value of the natural square root
- *
- * Return: Natural square root of n if found, -1 otherwise
+ * _sqrt - Recursive helper function for _sqrt_recursion.
+ * @n: The number to compute the square root of.
+ * @i: The lowest possible square root of n.
+ * Return: If n has a natural square root, return the square root.
+ *         If n does not have a natural square root, return -1.
  */
-int sqrt_helper(int n, int low, int high)
+int _sqrt(int n, int i)
 {
-	int mid;
+	int mid = i * i;
 
-	if (low > high)
-		return (-1); /* n does not have a natural square root */
-	mid = (low + high) / 2;
-	if (mid * mid == n)
-	return (mid); /* Found the natural square root */
-	else if (mid * mid > n)
-	return (sqrt_helper(n, low, mid - 1));
-	/* Search in the lower half of the range */
-	else
-	return (sqrt_helper(n, mid + 1, high));
-	/* Search in the upper half of the range */
+	if (mid > n)
+	{
+		return (-1);
+	}
+	if (mid  == n)
+	{
+		return (i);
+	}
+	return (_sqrt(n, i + 1));
 }
-
